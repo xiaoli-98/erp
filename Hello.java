38c4d15ebcd1ae48d251098660021ef55642672d
Mr.zhang <3177259856@qq.com>
2021-03-15 08:48:33 +0800

    In Hello.java File Add Two Statement

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 Class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello Git!!!");
+
+		System.out.println("认真听讲的孩子!!!");
+		System.out.println("Hello everyone!!!");
 	}
 }
