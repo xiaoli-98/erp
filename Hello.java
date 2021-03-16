ddce51c4b663d7a41c4cc433a6906c4ec5c33896
Mr.Zhang <3177259856@qq.com>
2021-03-16 16:41:30 +0800

    change hello

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 Class Hello{
 	public static void main(String args[]){
 		//System.out.println("Hello Git!!!");
-		System.out.println("认真听讲的孩子!!!");
-		System.out.println("Hello everyone!!!");
+		//System.out.println("认真听讲的孩子!!!");
+		//System.out.println("Hello everyone!!!");
 		System.out.println("明天要下雨了。。。");
 		System.out.println("这是我们的家!!!");
 		System.out.println("这是我们的家!!!");
