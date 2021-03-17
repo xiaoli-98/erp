d4230090f90967234045da4ac994153b0bf02fbd
Mr.Zhang <3177259856@qq.com>
2021-03-17 09:44:47 +0800

    Change Admin and Emp.java in dev

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 Class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello Git!!!");
+		//System.out.println("Hello Git!!!");
 
 		System.out.println("认真听讲的孩子!!!");
 		System.out.println("Hello everyone!!!");
